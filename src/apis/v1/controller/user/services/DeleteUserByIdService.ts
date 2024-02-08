7243dfa995f98b02d4c73c05104fd13b5ae860db
Eduardo De Patta <eduardo.depatta@gmail.com>
2024-02-08 19:56:22 -0300

    feat: create DeleteUserByIdService

---
+++ src/apis/v1/controller/user/services/DeleteUserByIdService.ts
@@ -0,0 +1,22 @@
+import { HTTP } from "../../../../../helpers/HttpRequest"
+import { catchAsync } from "../../../../../helpers/catchAsync"
+import {  deleteById } from "../../../../../helpers/genericDao"
+import { validateMissingParam } from "../../../../../helpers/validators"
+import { checkIfUserExistsUsecase } from "../usecase"
+
+const deleteUserByIdService = catchAsync(async (req: HTTP.Req<void, { idUsuario: number }>) => {
+  validateMissingParam(req.params, ['idUsuario'])
+  const { idUsuario } = req.params
+
+  await checkIfUserExistsUsecase(idUsuario)
+
+  const users = await deleteById('users', idUsuario)
+
+  return {
+    status: 200,
+    message: 'User successfully removed.',
+    data: users
+  }
+})
+
+export { deleteUserByIdService }
