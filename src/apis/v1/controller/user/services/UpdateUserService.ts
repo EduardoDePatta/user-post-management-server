2c66951a943b0a06b83d4eb057a1c095aa2bf1f4
Eduardo De Patta <eduardo.depatta@gmail.com>
2024-02-08 19:56:36 -0300

    feat: create UpdateUserService

---
+++ src/apis/v1/controller/user/services/UpdateUserService.ts
@@ -0,0 +1,23 @@
+import { HTTP } from "../../../../../helpers/HttpRequest"
+import { catchAsync } from "../../../../../helpers/catchAsync"
+import { updateInTable } from "../../../../../helpers/genericDao"
+import { validateMissingParam } from "../../../../../helpers/validators"
+import { IUser } from "../model/IUser"
+import { checkIfUserExistsUsecase } from "../usecase"
+
+const updateUserService = catchAsync<IUser>(async (req: HTTP.Req<Omit<IUser, 'id'>, { idUsuario: number }>) => {
+  validateMissingParam(req.params, ['idUsuario'])
+  const { email, first_name, last_name, login, password } = req.body
+  const { idUsuario } = req.params
+
+  await checkIfUserExistsUsecase(idUsuario)
+
+  const updatedUser = await updateInTable('users', idUsuario, { email, first_name, last_name, login, password })
+
+  return {
+    status: 201,
+    message: 'User successfully created.',
+    data: updatedUser
+  }
+})
+export { updateUserService }
